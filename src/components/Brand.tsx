e7396019e89ab891e27d10beb4f3acf836b9d375
Marcos <marck.vinisv@outlook.com>
2023-08-15 00:24:25 -0300

    .

---
+++ src/components/Brand.tsx
@@ -0,0 +1,5 @@
+﻿export function Brand () {
+  return (
+    <h1 className="pt-3 px-4 py-0 font-bold cursor-pointer text-3xl">Tércio</h1>
+  )
+}
---
+++ src/components/Line.tsx
@@ -0,0 +1,5 @@
+﻿export function Line () {
+  return (
+    <div className="border-b border-b-gray-700 opacity-30"/>
+  )
+}
